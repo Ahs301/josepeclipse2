cb9e1751f7e6ff0852a54a56c43aaf4418516240
JosepCervera <josepcf1dam@gmail.com>
2024-12-19 14:40:30 +0100

    Subida final

--- Proyecto Prueba/src/Principal.java
+++ Proyecto Prueba/src/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Holi wankenobi");
 		System.out.println("Agur yogurt!");
-		System.out.printlon("Ciao Pescao");
+		System.out.println("Ciao Pescao");
+		System.out.println("Adios de verdad!");
 	}
 
 }
